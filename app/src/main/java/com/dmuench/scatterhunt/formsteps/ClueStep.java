c4e935b8afe95f44a61263f968a7beac6d7b5422
Daniel Logerstedt <dl@Daniels-MBP-3.localdomain>
2019-02-05 12:13:38 -0800

    built Form for creating new goals, also fixed login/logout functionality

---
+++ app/src/main/java/com/dmuench/scatterhunt/formsteps/ClueStep.java
@@ -0,0 +1,104 @@
+package com.dmuench.scatterhunt.formsteps;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+
+import ernestoyaquello.com.verticalstepperform.Step;
+
+public class ClueStep extends Step<String> {
+
+    private EditText clueView;
+    private String clueNumber;
+
+    public ClueStep(String stepClue, String clueNumber) {
+        super(stepClue);
+        this.clueNumber = clueNumber;
+    }
+
+    @Override
+    protected View createStepContentLayout() {
+        // Here we generate the view that will be used by the library as the content of the step.
+        // In this case we do it programmatically, but we could also do it by inflating an XML layout.
+        clueView = new EditText(getContext());
+        clueView.setSingleLine(true);
+        clueView.setHint("ScatterGoal Clue " + clueNumber);
+
+        clueView.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Whenever the user updates the user name text, we update the state of the step.
+                // The step will be marked as completed only if its data is valid, which will be
+                // checked with a call to isStepDataValid().
+                markAsCompletedOrUncompleted(true);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        return clueView;
+    }
+
+    @Override
+    protected IsDataValid isStepDataValid(String stepData) {
+        // The step's data (i.e., the user name) will be considered valid only if it is longer than
+        // three characters. In case it is not, we will display an error message for feedback.
+        // In an optional step, you should implement this method to always return a valid value.
+        boolean isNameValid = stepData.length() >= 3;
+        String errorMessage = !isNameValid ? "3 characters minimum" : "";
+
+        return new IsDataValid(isNameValid, errorMessage);
+    }
+
+    @Override
+    public String getStepData() {
+        // We get the step's data from the value that the user has typed in the EditText view.
+        Editable clue = clueView.getText();
+        return clue != null ? clue.toString() : "";
+    }
+
+    @Override
+    public String getStepDataAsHumanReadableString() {
+        // Because the step's data is already a human-readable string, we don't need to convert it.
+        // However, we return "(Empty)" if the text is empty to avoid not having any text to display.
+        // This string will be displayed in the subclue of the step whenever the step gets closed.
+        String clue = getStepData();
+        return !clue.isEmpty() ? clue : "(Empty)";
+    }
+
+    @Override
+    protected void onStepOpened(boolean animated) {
+        // This will be called automatically whenever the step gets opened.
+    }
+
+    @Override
+    protected void onStepClosed(boolean animated) {
+        // This will be called automatically whenever the step gets closed.
+    }
+
+    @Override
+    protected void onStepMarkedAsCompleted(boolean animated) {
+        // This will be called automatically whenever the step is marked as completed.
+    }
+
+    @Override
+    protected void onStepMarkedAsUncompleted(boolean animated) {
+        // This will be called automatically whenever the step is marked as uncompleted.
+    }
+
+    @Override
+    public void restoreStepData(String stepData) {
+        // To restore the step after a configuration change, we restore the text of its EditText view.
+        clueView.setText(stepData);
+    }
+}
---
+++ app/src/main/java/com/dmuench/scatterhunt/formsteps/TitleStep.java
@@ -0,0 +1,102 @@
+package com.dmuench.scatterhunt.formsteps;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+
+import ernestoyaquello.com.verticalstepperform.Step;
+
+public class TitleStep extends Step<String> {
+
+    private EditText titleView;
+
+    public TitleStep(String stepTitle) {
+        super(stepTitle);
+    }
+
+    @Override
+    protected View createStepContentLayout() {
+        // Here we generate the view that will be used by the library as the content of the step.
+        // In this case we do it programmatically, but we could also do it by inflating an XML layout.
+        titleView = new EditText(getContext());
+        titleView.setSingleLine(true);
+        titleView.setHint("ScatterGoal Title");
+
+        titleView.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Whenever the user updates the user name text, we update the state of the step.
+                // The step will be marked as completed only if its data is valid, which will be
+                // checked with a call to isStepDataValid().
+                markAsCompletedOrUncompleted(true);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        return titleView;
+    }
+
+    @Override
+    protected IsDataValid isStepDataValid(String stepData) {
+        // The step's data (i.e., the user name) will be considered valid only if it is longer than
+        // three characters. In case it is not, we will display an error message for feedback.
+        // In an optional step, you should implement this method to always return a valid value.
+        boolean isNameValid = stepData.length() >= 3;
+        String errorMessage = !isNameValid ? "3 characters minimum" : "";
+
+        return new IsDataValid(isNameValid, errorMessage);
+    }
+
+    @Override
+    public String getStepData() {
+        // We get the step's data from the value that the user has typed in the EditText view.
+        Editable title = titleView.getText();
+        return title != null ? title.toString() : "";
+    }
+
+    @Override
+    public String getStepDataAsHumanReadableString() {
+        // Because the step's data is already a human-readable string, we don't need to convert it.
+        // However, we return "(Empty)" if the text is empty to avoid not having any text to display.
+        // This string will be displayed in the subtitle of the step whenever the step gets closed.
+        String title = getStepData();
+        return !title.isEmpty() ? title : "(Empty)";
+    }
+
+    @Override
+    protected void onStepOpened(boolean animated) {
+        // This will be called automatically whenever the step gets opened.
+    }
+
+    @Override
+    protected void onStepClosed(boolean animated) {
+        // This will be called automatically whenever the step gets closed.
+    }
+
+    @Override
+    protected void onStepMarkedAsCompleted(boolean animated) {
+        // This will be called automatically whenever the step is marked as completed.
+    }
+
+    @Override
+    protected void onStepMarkedAsUncompleted(boolean animated) {
+        // This will be called automatically whenever the step is marked as uncompleted.
+    }
+
+    @Override
+    public void restoreStepData(String stepData) {
+        // To restore the step after a configuration change, we restore the text of its EditText view.
+        titleView.setText(stepData);
+    }
+}
