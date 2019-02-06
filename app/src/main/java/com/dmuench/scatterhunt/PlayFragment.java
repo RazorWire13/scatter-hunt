07be2b2df1fe1f4aa707ef6f9399a4084e8e961f
mason bassett <bassettmason@gmail.com>
2019-02-06 14:34:26 -0800

    distance algo done

--- app/src/main/java/com/dmuench/scatterhunt/PlayFragment.java
+++ app/src/main/java/com/dmuench/scatterhunt/PlayFragment.java
@@ -41,7 +41,7 @@ findViewById to get any View inside the layout*/
         textView.setText("It Works!!");
 
         //This will create 5 items
-        item.createSubItems(5);
+        item.createSubItems(3);
         item.setIndicatorColorRes(R.color.white);
         item.setIndicatorIconRes(R.drawable.goal_expander_icon);
 
@@ -55,6 +55,23 @@ findViewById to get any View inside the layout*/
 
         return view;
 
+    }
+
+    private static double distance(double lat1, double lon1, double lat2, double lon2, String unit) {
+        if ((lat1 == lat2) && (lon1 == lon2)) {
+            return 0;
+        }
+        else {
+            double theta = lon1 - lon2;
+            double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
+            dist = Math.acos(dist);
+            dist = Math.toDegrees(dist);
+            dist = dist * 60 * 1.1515;
+            if (unit == "K") {
+                dist = dist * 1.609344;
+            }
+            return (dist);
+        }
     }
 
 }
